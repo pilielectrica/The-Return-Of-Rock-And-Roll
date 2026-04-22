05cf3d55a61d008a7215d87c83c987893f278f6a
pilielectrica <pilarmarco.musica@gmail.com>
2026-04-22 14:41:17 -0300

    assets y empieza a pintarse el escenario

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,25 +1,31 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@onready var sprite = $AnimatedSprite2D
+const SPEED = 200.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var direction = Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		velocity = direction * SPEED
+		
+		# Animaciones según dirección
+		if abs(direction.x) > abs(direction.y):
+			# Movimiento horizontal
+			sprite.play("run")
+			sprite.flip_h = direction.x < 0
+		else:
+			# Movimiento vertical
+			if direction.y > 0:
+				sprite.play("walk down")
+			else:
+				sprite.play("walk up")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = Vector2.ZERO
+		sprite.play("idle")
 
 	move_and_slide()
