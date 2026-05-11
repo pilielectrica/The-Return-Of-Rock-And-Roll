f6e6664e37144a3d446c02bd3259c895823e6c5b
pilielectrica <pilarmarco.musica@gmail.com>
2026-05-11 16:32:37 -0300

    enemigo se mueve joya

--- enemy_mummy_soldier.gd
+++ enemy_mummy_soldier.gd
@@ -6,9 +6,11 @@ extends CharacterBody2D
 var direction
 var distance
 var moving = true
+
 @onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta):
+	var pos_dif = player.position - position
 	if (moving):
 		direction = (player.position - position).normalized()
 		distance = position.distance_to(player.position)
@@ -17,3 +19,15 @@ func _physics_process(delta):
 		if (distance < 5):
 			moving = false
 			velocity = Vector2.ZERO
+		if (abs(pos_dif.x) > abs(pos_dif.y)):
+			if ((direction.x > 0)):
+				anim.play(" walk left or right")
+				anim.flip_h = false
+			elif (direction.x < 0):
+				anim.play(" walk left or right")
+				anim.flip_h = true
+		elif (abs(pos_dif.y) > abs(pos_dif.x)):
+			if (direction.y < 0):
+				anim.play("walk up")
+			elif (direction.y > 0):
+				anim.play("walk down")
