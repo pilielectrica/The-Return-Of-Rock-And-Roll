ceba992433fbf637c015af01fe75a94184797013
pilielectrica <pilarmarco.musica@gmail.com>
2026-07-07 16:05:45 -0300

    VAMOS ARGENTINAA TE AMMO MESSI

--- estatua.gd
+++ estatua.gd
@@ -4,11 +4,12 @@ extends RigidBody2D
 @onready var anim = $AnimatedSprite2D
 @export var barrera : CollisionShape2D
 @export var flip_h := false
-
+@export var pos_offset := 20
 func _ready() -> void:
 	collider.body_entered.connect(_on_collider_enter)
 	anim.flip_h = flip_h
 func _on_collider_enter(body):
 	if body.is_in_group("player"):
 		anim.play("block")
+		anim.position.x += pos_offset
 		barrera.set_deferred("disabled", false)
